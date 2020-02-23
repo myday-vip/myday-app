57d47640204e348f19443d3c414663d8dbdd764e
ifuture-pro <strongrobot@163.com>
2020-02-23 23:25:54 +0800

    index

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 https://myday.vip
 
 我们的每一天都应该过的有意义
+
+Dev Log
+* 2020.2.23 提交 APP 草稿
--- pages/today/index.vue
+++ pages/today/index.vue
@@ -1,14 +1,17 @@
 <template name="today">
 	<view>
 		<scroll-view scroll-y class="page">
-			<image src="/static/index.jpg"
+			<image src="/static/myday-banner2.png"
 			 mode="widthFix" class="response"></image>
 			<view class="nav-list" style="padding: 0rpx;">
 				<view class="cu-bar search bg-white" style="width: 100%;">
 					<view class="search-form round">
 						<text class="cuIcon-read"></text>
-						<input :adjust-position="false" type="text" placeholder="记录今天的输入、输出、体能" confirm-type="search"></input>
+						<input :adjust-position="false" type="text" placeholder="记录今天的输入、输出、体能" @focus="toggleAddStone"></input>
 					</view>
+				</view>
+				<view v-show="addStyle">
+					add event
 				</view>
 				<view class="cu-list menu" style="width: 100%;">
 					<view class="cu-bar bg-white solid-bottom margin-top">
@@ -25,7 +28,10 @@
 							<view class="content">
 								<view :class="item.status == 'TODO'?'text-orange':'text-gray'">
 									<view class="text-cut">{{item.name}}</view>
-									<view class="cu-tag round bg-orange sm">{{listItemType(item.type)}}</view>
+									<view v-if="item.type == 'EVERYDAY'" class="cu-tag round bg-orange sm">每日必做</view>
+									<view v-else-if="item.type == 'WEEKLY'" class="cu-tag round bg-green sm">每周必做</view>
+									<view v-else-if="item.type == 'MONTHLY'" class="cu-tag round bg-gradual-green sm">每月必做</view>
+									<view v-else class="cu-tag round bg-blue sm">今日计划</view>
 								</view>
 								<view class="text-gray text-sm flex"> 
 									<view class="text-cut" >{{item.text}}</view>
@@ -33,11 +39,16 @@
 							</view>
 							<view class="action">
 								<view class="text-grey text-xs">{{item.timeDo}}</view>
-								<checkbox class="round" :class="item.status == 'TODO'?'':'checked'" :checked="item.status == 'TODO'?false:true"></checkbox>
+								<view class="flex solid-bottom justify-end">
+									<view v-if="item.status == 'FULFILL'" class="padding-xs margin-xs radius cuIcon-favorfill text-green text-sl"></view>
+									<view v-if="item.status == 'FULFILL'" class="padding-xs margin-xs radius cuIcon-favorfill text-green text-sl"></view>
+									<view v-else class=" padding-sm margin-xs radius cuIcon-favor"></view>
+								</view>
+								
 							</view>
 							<view class="move">
 								<view class="bg-grey">置顶</view>
-								<view class="bg-red">删除</view>
+								<view class="bg-gradual-orange">完成</view>
 							</view>
 						</view>
 					</view>
@@ -61,37 +72,40 @@
 				},{
 					name: "英语口语",
 					classify: "INPUT",
-					type: "EVERYDAY",
+					type: "MONTHLY",
 					status: "TODO"
 				},{
 					name: "公众号文章",
 					text: "在我的公众号是输出高质量文章",
 					classify: "OUTPUT",
 					type: "WEEKLY",
-					status: "fulfill",
+					status: "FULFILL",
 					timeDo: "8:30"
+				},{
+					name: "读《人类简史》",
+					text: "才读了2章，感觉还不错。过几天写个读后感",
+					classify: "INPUT",
+					type: "GENERIC",
+					status: "FULFILL",
+					timeDo: "9:30"
 				}],
 				modalName: null,
 				listTouchStart: 0,
 				listTouchDirection: null,
+				addStyle: false
+				
 			};
 		},
 		computed: {
 			
 		},
+		watch:{
+		},
 		methods:{
-			listItemType(type){
-				let result;
-				if (type == "EVERYDAY") {
-					result = "每日必做"
-				}else if (type == "WEEKLY"){
-					result = "每周必做"
-				}else{
-					result = "今日计划"
-				}
-				return result
+			//打开添加事件界面
+			toggleAddStone(e){
+				this.addStyle = true
 			},
-			
 			// ListTouch触摸开始
 			ListTouchStart(e) {
 				this.listTouchStart = e.touches[0].pageX
