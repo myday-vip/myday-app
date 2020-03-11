f86ac101cd769f3c1efe054724725a0907d83315
ifuture-pro <strongrobot@163.com>
2020-03-11 21:48:52 +0800

    event show in timeline

--- pages/life/timeline.vue
+++ pages/life/timeline.vue
@@ -8,17 +8,61 @@
 			<view class="cu-item" 
 			:class="subitem.classify == 'INPUT'?'text-orange':(subitem.classify == 'OUTPUT'?'text-green':'text-red')"
 			v-for="(subitem,subindex) in item" :key="subindex">
-				<view class="content">
-					<view class="cu-capsule radius">
-						<view class="cu-tag bg-cyan">{{subitem.updateTime | fomate3}}</view>
-						<view class="cu-tag line-cyan">{{subitem.updateTime | fomate2}}</view>
+				<view class="content" style="background-color: #f4f8f9;">
+					<view class="flex solid-bottom justify-between">
+						<view class="nav-title">{{subitem.subject}}</view>
+						<view class="cu-capsule radius">
+							<view class="cu-tag bg-cyan">{{subitem.updateTime | fomate3}}</view>
+							<view class="cu-tag line-cyan">{{subitem.updateTime | fomate2}}</view>
+						</view>
 					</view>
-					<view class="margin-top">{{subitem.subject}}</view>
-					<view class="margin-top">{{subitem.text}}</view>
+					<view class="desc">
+						<view class="text-content"> {{subitem.text || ""}}</view>
+						<!-- 图片 -->
+						<block v-if="subitem.show2.length > 0">
+							<view class="grid flex-sub col-3 grid-square" style="padding-top: 30rpx;">
+								<view v-for="(ssitem,ssindex) in subitem.show2" :key="ssindex" 
+								class="bg-img" 
+								:style="{backgroundImage:'url(http://localhost:8080/' + ssitem.val1+ ')'}"
+								@tap="showmodel(ssitem)">
+								</view>
+							</view>
+						</block>
+						<!-- 位置 链接-->
+						<block v-if="subitem.show.length > 0">
+							<view v-for="(ssitem,ssindex) in subitem.show" :key="ssindex">
+								<view v-if="ssitem.type == 'LOCATION'">
+									<text class="margin-right-xs cuIcon-locationfill"></text>
+									<text>{{ssitem.val1}}</text>
+								</view>
+								<view v-if="ssitem.type == 'LINK'">
+									<text class="margin-right-xs cuIcon-link"></text>
+									<text>{{ssitem.value}}</text>
+								</view>
+							</view>
+						</block>
+						
+						<view>
+							<view v-if="subitem.classify == 'INPUT'" class="cu-tag bg-red light sm round">输入</view>
+							<view v-if="subitem.classify == 'OUTPUT'" class="cu-tag bg-green light sm round">输出</view>
+							<view v-if="subitem.classify == 'PHYSICAL_AGILITY'" class="cu-tag bg-gradual-orange light sm round">体能</view>
+							<view v-if="subitem.type == 'EVERYDAY'" class="cu-tag bg-green light sm round">每日必做</view>
+							<view v-if="subitem.type == 'WEEKLY'" class="cu-tag bg-green light sm round">每周必做</view>
+							<view v-if="subitem.type == 'MONTHLY'" class="cu-tag bg-green light sm round">每月必做</view>
+						</view>
+					</view>
 				</view>
 			</view>
 
 		</view>
+		
+		
+		<view class="cu-modal" id="imgModal" :class="modalName?'show':''" @touchstart="hideModal">
+			<view class="cu-dialog" >
+				<image :src="'http://localhost:8080/' + bigImg.url" mode="aspectFit"></image>
+			</view>
+		</view>
+		
 	</view>
 </template>
 
@@ -28,6 +72,13 @@
 	export default {
 		data() {
 			return {
+				modalName: null,
+				bigImg: {
+					url:null,
+					width:0,
+					height:0
+				},
+				updatedNumber:0,
 				data:[],
 				currentDate: null//点击进来的时间 e.g:2020-01-01
 			};
@@ -37,18 +88,23 @@
 				this.fetchData(option.date)
 			}
 		},
-		mounted: function () {
-		  this.$nextTick(function () {
-			uni.createSelectorQuery().select(".cu-timeline").boundingClientRect(data=>{
-				uni.createSelectorQuery().select(".current").boundingClientRect((res)=>{
-					uni.pageScrollTo({
-						duration:2000,
-						scrollTop:res.top - data.top
-					})
-				}).exec()
-			}).exec();
-		  })
-		},
+		updated: function () {
+			if (this.updatedNumber == 1){
+				return
+			}
+			var _this = this
+			this.$nextTick(function () {
+				uni.createSelectorQuery().select(".cu-timeline").boundingClientRect(data=>{
+					uni.createSelectorQuery().select(".current").boundingClientRect((res)=>{
+						uni.pageScrollTo({
+							duration:2000,
+							scrollTop:res.top - data.top
+						})
+						_this.updatedNumber = 1
+					}).exec()
+				}).exec();
+				})
+			},
 		filters:{
 			fomate1: function(time) {
 				var date = new Date(time)
@@ -81,6 +137,19 @@
 			}
 		},
 		methods:{
+			hideModal: function(e){
+				if(e && e.target.id == 'imgModal'){
+					this.modalName = null
+				}
+				
+			},
+			showmodel: function(data){
+				this.modalName = "image"
+				this.bigImg.url = data.val2
+				this.bigImg.width = data.val3
+				this.bigImg.height = data.val4
+				console.log(this.bigImg)
+			},
 			fetchData: function(date) {
 				this.currentDate = date
 				getEventTimeLine(date).then((res) => {
@@ -100,5 +169,16 @@
 </script>
 
 <style>
-
+.cu-dialog {
+	position: relative;
+	display: inline-block;
+	vertical-align: middle;
+	margin-left: auto;
+	margin-right: auto;
+	width: 100%;
+	max-width: 100%;
+	background-color:transparent;
+	border-radius: 10upx;
+	overflow: hidden;
+}
 </style>
